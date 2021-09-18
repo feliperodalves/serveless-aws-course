f323a58b468be1fad37deb5b5ff9429eae080efe
Felipe Alves <felipe.rod.alves@gmail.com>
2021-09-18 16:21:16 -0300

    feature: pequenas modificações modulo 03

--- 003-image-analysis/image-analysis/handler.js
+++ 003-image-analysis/image-analysis/handler.js
@@ -1,97 +1,99 @@
 'use strict';
-const { get } = require('axios')
+const { get } = require('axios');
 
 class Handler {
   constructor({ rekoSvc, translatorSvc }) {
-    this.rekoSvc = rekoSvc
-    this.translatorSvc = translatorSvc
+    this.rekoSvc = rekoSvc;
+    this.translatorSvc = translatorSvc;
   }
 
   async detectImageLabels(buffer) {
-    const result = await this.rekoSvc.detectLabels({
-      Image: {
-        Bytes: buffer
-      }
-    }).promise()
+    const result = await this.rekoSvc
+      .detectLabels({
+        Image: {
+          Bytes: buffer,
+        },
+      })
+      .promise();
 
-    const workingItems = result.Labels.filter(({ Confidence }) => Confidence > 80)
+    const workingItems = result.Labels.filter(({ Confidence }) => Confidence > 80);
 
-    const names = workingItems.map(({ Name }) => Name).join(' and ')
+    const names = workingItems.map(({ Name }) => Name).join(' and ');
 
-    return { names, workingItems }
+    return { names, workingItems };
   }
 
   async translateText(text) {
     const params = {
       SourceLanguageCode: 'en',
       TargetLanguageCode: 'pt',
-      Text: text
-    }
-    const { TranslatedText } = await this.translatorSvc.translateText(params).promise()
+      Text: text,
+    };
+    const { TranslatedText } = await this.translatorSvc.translateText(params).promise();
 
-    return TranslatedText.split(' e ')
+    return TranslatedText.split(' e ');
   }
 
   formatTextResults(texts, workingItems) {
-    const finalText = []
+    const finalText = [];
     for (const indexText in texts) {
-      const nameInPortuguese = texts[indexText]
-      const confidence = workingItems[indexText].Confidence
-      finalText.push(` ${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`)
+      const nameInPortuguese = texts[indexText];
+      const confidence = workingItems[indexText].Confidence;
+      finalText.push(` ${confidence.toFixed(2)}% de ser do tipo ${nameInPortuguese}`);
     }
 
-    return finalText.join('\n')
+    return finalText.join('\n');
   }
 
   async getImageBuffer(imageURL) {
     const response = await get(imageURL, {
-      responseType: 'arraybuffer'
-    })
-    const buffer = Buffer.from(response.data, 'base64')
+      responseType: 'arraybuffer',
+    });
+    const buffer = Buffer.from(response.data, 'base64');
 
-    return buffer
+    return buffer;
   }
 
   async main(event) {
     try {
-      const { imageUrl } = event.queryStringParameters
-      console.log(imageUrl)
+      const { imageUrl } = event.queryStringParameters;
+      console.log(imageUrl);
 
-      console.log('Downloading image...')
-      const imgBuffer = await this.getImageBuffer(imageUrl)
+      console.log('Downloading image...');
+      const imgBuffer = await this.getImageBuffer(imageUrl);
 
-      console.log('Detecting labels...')
-      const { names, workingItems } = await this.detectImageLabels(imgBuffer)
+      console.log('Detecting labels...');
+      const { names, workingItems } = await this.detectImageLabels(imgBuffer);
 
-      console.log('Translating to Portuguese...')
-      const texts = await this.translateText(names)
+      console.log('Translating to Portuguese...');
+      const texts = await this.translateText(names);
 
-      console.log('Handling final object...')
-      const finalText = this.formatTextResults(texts, workingItems)
+      console.log('Handling final object...');
+      const finalText = this.formatTextResults(texts, workingItems);
 
-      console.log('Finishing...')
+      console.log('Finishing...');
 
       return {
         statusCode: 200,
-        body: `A imagem tem\n`.concat(finalText)
-      }
-
+        body: `A imagem tem\n`.concat(finalText),
+      };
     } catch (error) {
-      console.log('Error---', error.stack)
+      console.log('Error---', error.stack);
       return {
-        statuscode: 500,
-        body: 'Internal server error!'
-      }
+        statusCode: 500,
+        body: 'Internal server error!',
+      };
     }
   }
 }
 
-const aws = require('aws-sdk')
-const reko = new aws.Rekognition()
-const translator = new aws.Translate()
+// factory
+const aws = require('aws-sdk');
+const reko = new aws.Rekognition();
+const translator = new aws.Translate();
 const handler = new Handler({
   rekoSvc: reko,
-  translatorSvc: translator
-})
+  translatorSvc: translator,
+});
 
-module.exports.main = handler.main.bind(handler)
+module.exports.main = handler.main.bind(handler);

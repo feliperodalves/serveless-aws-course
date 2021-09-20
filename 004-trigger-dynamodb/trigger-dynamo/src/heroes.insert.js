fd438abb8e83407c83ba7905689c34abd264b161
Felipe Alves <felipe.rod.alves@gmail.com>
2021-09-20 01:01:51 -0300

    feature: conectando com dynamodb

--- 004-trigger-dynamodb/trigger-dynamo/src/heroes.insert.js
+++ 004-trigger-dynamodb/trigger-dynamo/src/heroes.insert.js
@@ -1,19 +1,61 @@
+const uuid = require('uuid');
+
 class Handler {
-  async main() {
+  constructor({ dynamoDBSvc }) {
+    this.dynamoDBSvc = dynamoDBSvc;
+    this.dynamoDBTable = process.env.DYNAMODB_TABLE;
+  }
+
+  insertItem(data) {
+    return this.dynamoDBSvc.put(data).promise();
+  }
+
+  prepareData(dbParameters) {
+    const params = {
+      TableName: this.dynamoDBTable,
+      Item: {
+        ...dbParameters,
+        id: uuid.v1(),
+        createdAt: new Date().toISOString(),
+      },
+    };
+    return params;
+  }
+
+  handlerSuccess(responseData) {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify(responseData),
+    };
+    return response;
+  }
+
+  handlerError(responseData) {
+    const response = {
+      statusCode: responseData.statusCode || 501,
+      headers: { 'Content-Type': 'text/plain' },
+      body: `Couldn't create item!`,
+    };
+    return response;
+  }
+
+  async main(event) {
     try {
-      return {
-        statusCode: 200,
-        body: 'Hello',
-      };
+      const data = JSON.parse(event.body);
+      const dbParams = this.prepareData(data);
+      await this.insertItem(dbParams);
+      return this.handlerSuccess(dbParams.Item);
     } catch (error) {
-      return {
-        statusCode: 500,
-        body: 'Internal Error',
-      };
+      console.error('Deu ruim --- ', error.stack);
+      return this.handlerError({ statuscode: 500 });
     }
   }
 }
 
 //factory
-const handler = new Handler();
+const AWS = require('aws-sdk');
+const dynamoDB = AWS.DynamoDB.DocumentClient();
+const handler = new Handler({
+  dynamoDBSvc: dynamoDB,
+});
 module.exports = handler.main.bind(handler);
